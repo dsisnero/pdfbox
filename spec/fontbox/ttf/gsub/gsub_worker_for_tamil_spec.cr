4d688deefacfb461f1bac7a8bb1d936c26d80cf0
Dominic Sisneros <dsisnero@gmail.com>
2026-02-11 18:23:25 -0700

    Port GsubWorkerForTamilTest.java integration test
    - Add dummy test that expects DefaultGsubWorker (matching Java TODO)
    - Tamil worker implementation exists but factory returns DefaultGsubWorker
    - All GSUB tests now pass (61 examples, 8 pending)

---
+++ spec/fontbox/ttf/gsub/gsub_worker_for_tamil_spec.cr
@@ -0,0 +1,35 @@
+module Fontbox::TTF::Gsub
+  LOHIT_TAMIL_TTF = "apache_pdfbox/fontbox/src/test/resources/ttf/Lohit-Tamil.ttf"
+
+  private def self.with_tamil_font(&)
+    font = TTFParser.new.parse(Pdfbox::IO::RandomAccessReadBufferedFile.new(LOHIT_TAMIL_TTF))
+    begin
+      yield font
+    ensure
+      font.close
+    end
+  end
+
+  private def self.get_tamil_glyph_ids(cmap_lookup, word : String) : Array(Int32)
+    original_glyph_ids = [] of Int32
+    word.each_char do |unicode_char|
+      glyph_id = cmap_lookup.get_glyph_id(unicode_char.ord)
+      glyph_id.should be > 0
+      original_glyph_ids << glyph_id
+    end
+    original_glyph_ids
+  end
+
+  describe "GsubWorkerForTamil" do
+    it "testDummy" do
+      with_tamil_font do |font|
+        cmap_lookup = font.get_unicode_cmap_lookup
+        gsub_worker = GsubWorkerFactory.new.get_gsub_worker(cmap_lookup, font.get_gsub_data)
+
+        # Java test expects DefaultGsubWorker because Tamil worker is not fully implemented
+        # (has TODO comment in Java source)
+        gsub_worker.should be_a(DefaultGsubWorker)
+      end
+    end
+  end
+end
