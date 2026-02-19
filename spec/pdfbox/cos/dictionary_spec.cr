77f063e02c6f2b217435dbbc77f5200232f60387
Dominic Sisneros <dsisnero@gmail.com>
2026-02-18 21:22:07 -0700

    Port COSDictionary test for dictionary-stream inequality

---
+++ spec/pdfbox/cos/dictionary_spec.cr
@@ -0,0 +1,20 @@
+require "../../spec_helper"
+
+describe Pdfbox::Cos::Dictionary do
+  describe "#==" do
+    it "is not equal to stream with identical dictionary entries" do
+      dictionary = Pdfbox::Cos::Dictionary.new
+      stream = Pdfbox::Cos::Stream.new
+
+      be_name = Pdfbox::Cos::Name.new("BE")
+      length_name = Pdfbox::Cos::Name.new("Length")
+
+      dictionary[be_name] = be_name
+      dictionary[length_name] = Pdfbox::Cos::Integer.get(0_i64)
+      stream[be_name] = be_name
+
+      (dictionary == stream).should be_false
+      (stream == dictionary).should be_false
+    end
+  end
+end
