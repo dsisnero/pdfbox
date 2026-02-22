c59971ea137749c19ae0b866e80a9951e1714ea9
Dominic Sisneros <dsisnero@gmail.com>
2026-02-22 12:51:16 -0700

    feat: Implement COSDictionaryMap wrapper class
    - Port COSDictionaryMap from Java with sync between Hash and Cos::Dictionary
    - Add convert and convert_basic_types_to_map static methods
    - Add comprehensive spec tests
    - Fix Cos::Dictionary key usage (must be Cos::Name)
    - Update existing COSDictionaryMap module to class

---
+++ spec/pdfbox/pdmodel/common/cosdictionary_map_spec.cr
@@ -0,0 +1,86 @@
+require "../../../spec_helper"
+
+module Pdfbox::Pdmodel::Common
+  class TestCOSObjectable
+    def cos_object : Cos::Base
+      Cos::String.new("value")
+    end
+  end
+
+  class WrapperTestCOSObjectable
+    def initialize(@value : String); end
+    def cos_object : Cos::Base
+      Cos::String.new(@value)
+    end
+  end
+
+  describe COSDictionaryMap do
+    it "converts basic types to map" do
+      dict = Cos::Dictionary.new
+      dict[Cos::Name.new("Name")] = Cos::Name.new("Test")
+      dict[Cos::Name.new("Integer")] = Cos::Integer.new(42)
+      dict[Cos::Name.new("Float")] = Cos::Float.new(3.14)
+      dict[Cos::Name.new("String")] = Cos::String.new("Hello")
+      dict[Cos::Name.new("Boolean")] = Cos::Boolean.get(true)
+
+      map = COSDictionaryMap.convert_basic_types_to_map(dict)
+      map.should_not be_nil
+      map = map.not_nil!
+      map["Name"].should eq "Test"
+      map["Integer"].should eq 42_i64
+      map["Float"].should eq 3.14
+      map["String"].should eq "Hello"
+      map["Boolean"].should eq true
+    end
+
+    it "converts map to dictionary" do
+      some_map = {
+        "key1" => TestCOSObjectable.new,
+        "key2" => TestCOSObjectable.new,
+      }
+      dict = COSDictionaryMap.convert(some_map)
+      dict.should be_a(Cos::Dictionary)
+      dict[Cos::Name.new("key1")].should be_a(Cos::String)
+      dict[Cos::Name.new("key2")].should be_a(Cos::String)
+    end
+
+    describe "wrapper" do
+      it "wraps a dictionary and syncs entries" do
+        dict = Cos::Dictionary.new
+        actuals = {} of String => WrapperTestCOSObjectable
+        wrapper = COSDictionaryMap(WrapperTestCOSObjectable).new(actuals, dict)
+        wrapper.size.should eq 0
+        wrapper.empty?.should be_true
+
+        obj = WrapperTestCOSObjectable.new("test")
+        wrapper["key"] = obj
+        wrapper.size.should eq 1
+        dict[Cos::Name.new("key")].should be_a(Cos::String)
+        actuals["key"].should eq obj
+      end
+
+      it "deletes entry" do
+        dict = Cos::Dictionary.new
+        dict[Cos::Name.new("key")] = Cos::String.new("value")
+        obj = WrapperTestCOSObjectable.new("value")
+        actuals = {"key" => obj}
+        wrapper = COSDictionaryMap(WrapperTestCOSObjectable).new(actuals, dict)
+        wrapper.delete("key").should eq obj
+        dict.has_key?(Cos::Name.new("key")).should be_false
+        actuals.has_key?("key").should be_false
+      end
+
+      it "clears all entries" do
+        dict = Cos::Dictionary.new
+        dict[Cos::Name.new("key")] = Cos::String.new("value")
+        obj = WrapperTestCOSObjectable.new("value")
+        actuals = {"key" => obj}
+        wrapper = COSDictionaryMap(WrapperTestCOSObjectable).new(actuals, dict)
+        wrapper.clear
+        wrapper.size.should eq 0
+        dict.size.should eq 0
+        actuals.size.should eq 0
+      end
+    end
+  end
+end
--- src/pdfbox/pdmodel/common/cosdictionary_map.cr
+++ src/pdfbox/pdmodel/common/cosdictionary_map.cr
@@ -1,41 +1,131 @@
-# This module provides dictionary mapping utilities
-# that sync contents between Crystal Hashes and COSDictionaries
+# This is a Map that will automatically sync the contents to a COSDictionary.
 module Pdfbox::Pdmodel::Common
-  module COSDictionaryMap
-    # Convert a COSDictionary to a Hash with basic Ruby types
-    # (String, Integer, Float, Bool)
-    def self.convert_basic_types_to_map(dict : Cos::Dictionary?) : Hash(String, Object)?
-      return if dict.nil?
+  # Wrapper class that synchronizes a Hash with a COSDictionary.
+  # Keys are strings, values must respond to `#cos_object`.
+  class COSDictionaryMap(V)
+    @map : Cos::Dictionary
+    @actuals : Hash(String, V)
 
-      result = {} of String => Object
-      dict.entries.each do |key, value|
-        actual_object = case value
-                        when Cos::String
-                          value.value
-                        when Cos::Integer
-                          value.value
-                        when Cos::Float
-                          value.value
-                        when Cos::Name
-                          value.value
-                        when Cos::Boolean
-                          value.value
-                        else
-                          raise "Error: unknown type of object to convert: #{value.class}"
-                        end
-        result[key.value] = actual_object
-      end
-      result
+    # Constructor for this map.
+    # @param actuals_map The map with standard crystal objects as values.
+    # @param dic_map The map with COSBase objects as values.
+    def initialize(actuals_map : Hash(String, V), dic_map : Cos::Dictionary)
+      @actuals = actuals_map
+      @map = dic_map
     end
 
-    # Convert a Hash to a COSDictionary
-    def self.convert(some_map : Hash(String, _))
+    # Returns the size of the map.
+    def size : Int32
+      @map.size
+    end
+
+    # Checks if the map is empty.
+    def empty? : Bool
+      size == 0
+    end
+
+    # Checks if the map contains the given key.
+    def has_key?(key : String) : Bool
+      @actuals.has_key?(key)
+    end
+
+    # Checks if the map contains the given value.
+    def has_value?(value : V) : Bool
+      @actuals.has_value?(value)
+    end
+
+    # Gets the value for the given key, or nil if not present.
+    def [](key : String) : V?
+      @actuals[key]?
+    end
+
+    # Gets the value for the given key, raising if not present.
+    def fetch(key : String) : V
+      @actuals[key]
+    end
+
+    # Associates the given key with the given value.
+    # The value must respond to `cos_object` (COSObjectable).
+    def []=(key : String, value : V) : V
+      if value.responds_to?(:cos_object)
+        cos_obj = value.cos_object
+        @map[Cos::Name.new(key)] = cos_obj
+      else
+        raise "Value must be COSObjectable (responds to #cos_object)"
+      end
+      @actuals[key] = value
+    end
+
+    # Removes the entry for the given key.
+    def delete(key : String) : V?
+      @map.delete(Cos::Name.new(key))
+      @actuals.delete(key)
+    end
+
+    # Clears all entries.
+    def clear : Nil
+      @map.clear
+      @actuals.clear
+    end
+
+    # Returns the keys as an array.
+    def keys : Array(String)
+      @actuals.keys
+    end
+
+    # Returns the values as an array.
+    def values : Array(V)
+      @actuals.values
+    end
+
+    # Returns the underlying COSDictionary.
+    def to_dictionary : Cos::Dictionary
+      @map
+    end
+
+    # Equality check.
+    def ==(other : self) : Bool
+      @map == other.@map
+    end
+
+    # Hash code.
+    def_hash @map
+
+    # This will take a COS dictionary and convert it into COSDictionaryMap.
+    # All COS objects will be converted to their primitive form (String, Int64, Float64, Bool).
+    # Raises if unknown type.
+    def self.convert_basic_types_to_map(map : Cos::Dictionary?) : COSDictionaryMap(Bool | Int64 | Float64 | String)?
+      return unless map
+      actual_map = Hash(String, Bool | Int64 | Float64 | String).new
+      map.entries.each do |key, cos_obj|
+        actual_object = case cos_obj
+                        when Cos::String
+                          cos_obj.value
+                        when Cos::Integer
+                          cos_obj.value
+                        when Cos::Name
+                          cos_obj.value
+                        when Cos::Float
+                          cos_obj.value
+                        when Cos::Boolean
+                          cos_obj.value
+                        else
+                          raise "Error: unknown type of object to convert: #{cos_obj}"
+                        end
+        actual_map[key.value] = actual_object
+      end
+      new(actual_map, map)
+    end
+
+    # This will take a map<String, COSObjectable> and convert it into a COSDictionary.
+    def self.convert(some_map : Hash(String, T)) : Cos::Dictionary forall T
       dic = Cos::Dictionary.new
       some_map.each do |name, objectable|
         if objectable.responds_to?(:cos_object)
-          dic[Cos::Name.new(name)] = objectable.cos_object
+          cos_obj = objectable.cos_object
+          dic[Cos::Name.new(name)] = cos_obj
         else
-          raise "Error: object does not respond to cos_object: #{objectable.class}"
+          raise "Value must be COSObjectable (responds to #cos_object)"
         end
       end
       dic
