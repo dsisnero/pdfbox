011689a001ffa5c7c9bc7d609bb00a1c2659f36a
Dominic Sisneros <dsisnero@gmail.com>
2026-02-23 01:47:04 -0700

    Port TestParser.java for Type4 function parser tests

---
+++ spec/pdfbox/pdmodel/common/function/type4/parser_spec.cr
@@ -0,0 +1,38 @@
+require "../../../../../spec_helper"
+require "../../../../../helpers/type4_tester"
+
+module Pdfbox::Pdmodel::Common::Function::Type4
+  describe "Parser" do
+    it "tests parser basics" do
+      Spec::Helpers::Type4Tester.create("3 4 add 2 sub").pop(5).is_empty
+    end
+
+    it "tests nested blocks" do
+      Spec::Helpers::Type4Tester.create("true { 2 1 add } { 2 1 sub } ifelse")
+        .pop(3).is_empty
+      Spec::Helpers::Type4Tester.create("{ true }").pop(true).is_empty
+    end
+
+    describe "parseReal" do
+      it "parses float values" do
+        # Test parse_real method directly
+        InstructionSequenceBuilder.parse_real("0").should be_close(0.0_f32, 0.00001_f32)
+        InstructionSequenceBuilder.parse_real("1").should be_close(1.0_f32, 0.00001_f32)
+        InstructionSequenceBuilder.parse_real("+1").should be_close(1.0_f32, 0.00001_f32)
+        InstructionSequenceBuilder.parse_real("-1").should be_close(-1.0_f32, 0.00001_f32)
+        InstructionSequenceBuilder.parse_real("3.14157").should be_close(3.14157_f32, 0.00001_f32)
+        InstructionSequenceBuilder.parse_real("-1.2").should be_close(-1.2_f32, 0.00001_f32)
+        InstructionSequenceBuilder.parse_real("1.0E-5").should be_close(1.0e-5_f32, 0.00001_f32)
+      end
+    end
+
+    it "tests problematic functions from PDFBOX-804" do
+      # This is an example of a tint to CMYK function
+      # Problems here were:
+      # 1. no whitespace between "mul" and "}" (token was detected as "mul}")
+      # 2. line breaks cause endless loops
+      Spec::Helpers::Type4Tester.create("1 {dup dup .72 mul exch 0 exch .38 mul}\n")
+        .pop(0.38_f32).pop(0_f32).pop(0.72_f32).pop(1.0_f32).is_empty
+    end
+  end
+end
