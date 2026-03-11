e5d77aebf8c0370593d442b450dc53cbd3570763
Dominic Sisneros <dsisnero@gmail.com>
2026-03-11 10:44:24 -0600

    Port PDFBox Hex util and TestHexUtil parity cases

--- spec/porting/pdfbox-lxq_spec.cr
+++ spec/porting/pdfbox-lxq_spec.cr
@@ -30,4 +30,63 @@ describe "Porting parity pdfbox-lxq" do
   it "StringUtilTest#testTokenizeOnSpace_onlySpacesWithText" do
     Pdfbox::Util::StringUtil.tokenize_on_space("  a  ").should eq([" ", " ", "a", " ", " "])
   end
+
+  # Ported from TestHexUtil.java
+  it "TestHexUtil#testGetCharsFromShortWithoutPassingInABuffer" do
+    Pdfbox::Util::Hex.get_chars(0x0000_i16).should eq(['0', '0', '0', '0'])
+    Pdfbox::Util::Hex.get_chars(0x000F_i16).should eq(['0', '0', '0', 'F'])
+    Pdfbox::Util::Hex.get_chars(-21_555_i16).should eq(['A', 'B', 'C', 'D']) # 0xABCD as signed short
+    Pdfbox::Util::Hex.get_chars(-17_730_i16).should eq(['B', 'A', 'B', 'E']) # 0xBABE from 0xCAFEBABE
+  end
+
+  it "TestHexUtil#testGetCharsUTF16BE" do
+    Pdfbox::Util::Hex.get_chars_utf16be("ab").should eq(['0', '0', '6', '1', '0', '0', '6', '2'])
+    Pdfbox::Util::Hex.get_chars_utf16be("帮助").should eq(['5', 'E', '2', 'E', '5', '2', 'A', '9'])
+  end
+
+  it "TestHexUtil#testMisc" do
+    byte_src = Bytes.new(256) { |i| i.to_u8 }
+
+    byte_src.each_with_index do |_, i|
+      bytes = Pdfbox::Util::Hex.get_bytes(i.to_u8)
+      bytes.size.should eq(2)
+      expected = i.to_s(16, upcase: true).rjust(2, '0').to_slice
+      bytes.should eq(expected)
+      Pdfbox::Util::Hex.get_string(i.to_u8).to_slice.should eq(bytes)
+      Pdfbox::Util::Hex.decode_hex(Pdfbox::Util::Hex.get_string(i.to_u8)).should eq(Bytes[i.to_u8])
+    end
+
+    byte_dst = Pdfbox::Util::Hex.get_bytes(byte_src)
+    byte_dst.size.should eq(byte_src.size * 2)
+
+    dst_string = Pdfbox::Util::Hex.get_string(byte_src)
+    dst_string.size.should eq(byte_src.size * 2)
+    dst_string.to_slice.should eq(byte_dst)
+    Pdfbox::Util::Hex.decode_hex(dst_string).should eq(byte_src)
+  end
+
+  it "TestHexUtil#testGetHexValue" do
+    valid_hex = Set(Char).new
+
+    ('0'..'9').each do |c|
+      valid_hex << c
+      Pdfbox::Util::Hex.get_hex_value(c).should eq(c.to_s.to_i(16))
+    end
+    ('a'..'f').each do |c|
+      valid_hex << c
+      Pdfbox::Util::Hex.get_hex_value(c).should eq(c.to_s.to_i(16))
+    end
+    ('A'..'F').each do |c|
+      valid_hex << c
+      Pdfbox::Util::Hex.get_hex_value(c).should eq(c.to_s.to_i(16))
+    end
+    valid_hex.size.should eq(22)
+
+    (0..255).each do |c|
+      char = c.chr
+      unless valid_hex.includes?(char)
+        Pdfbox::Util::Hex.get_hex_value(char).should eq(-256)
+      end
+    end
+  end
 end
--- src/pdfbox/util.cr
+++ src/pdfbox/util.cr
@@ -49,4 +49,97 @@ module Pdfbox::Util
       tokens
     end
   end
+
+  # Ported from Apache PDFBox Hex.
+  module Hex
+    HEX_BYTES = Bytes['0'.ord.to_u8, '1'.ord.to_u8, '2'.ord.to_u8, '3'.ord.to_u8, '4'.ord.to_u8, '5'.ord.to_u8, '6'.ord.to_u8, '7'.ord.to_u8, '8'.ord.to_u8, '9'.ord.to_u8, 'A'.ord.to_u8, 'B'.ord.to_u8, 'C'.ord.to_u8, 'D'.ord.to_u8, 'E'.ord.to_u8, 'F'.ord.to_u8]
+    HEX_CHARS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+
+    def self.get_string(b : UInt8) : String
+      String.new(Bytes[HEX_BYTES[high_nibble(b)], HEX_BYTES[low_nibble(b)]])
+    end
+
+    def self.get_string(bytes : Bytes) : String
+      ascii = Bytes.new(bytes.size * 2)
+      bytes.each_with_index do |b, i|
+        ascii[i * 2] = HEX_BYTES[high_nibble(b)]
+        ascii[(i * 2) + 1] = HEX_BYTES[low_nibble(b)]
+      end
+      String.new(ascii)
+    end
+
+    def self.get_bytes(b : UInt8) : Bytes
+      Bytes[HEX_BYTES[high_nibble(b)], HEX_BYTES[low_nibble(b)]]
+    end
+
+    def self.get_bytes(bytes : Bytes) : Bytes
+      ascii = Bytes.new(bytes.size * 2)
+      bytes.each_with_index do |b, i|
+        ascii[i * 2] = HEX_BYTES[high_nibble(b)]
+        ascii[(i * 2) + 1] = HEX_BYTES[low_nibble(b)]
+      end
+      ascii
+    end
+
+    def self.get_chars(num : Int16) : Array(Char)
+      u = num.unsafe_as(UInt16)
+      [
+        HEX_CHARS[((u >> 12) & 0x0f).to_i],
+        HEX_CHARS[((u >> 8) & 0x0f).to_i],
+        HEX_CHARS[((u >> 4) & 0x0f).to_i],
+        HEX_CHARS[(u & 0x0f).to_i],
+      ]
+    end
+
+    def self.get_chars_utf16be(text : String) : Array(Char)
+      hex = Array(Char).new(text.size * 4)
+      text.each_char do |char|
+        c = char.ord
+        hex << HEX_CHARS[((c >> 12) & 0x0f).to_i]
+        hex << HEX_CHARS[((c >> 8) & 0x0f).to_i]
+        hex << HEX_CHARS[((c >> 4) & 0x0f).to_i]
+        hex << HEX_CHARS[(c & 0x0f).to_i]
+      end
+      hex
+    end
+
+    def self.decode_base64(base64_value : String) : Bytes
+      Base64.decode(StringUtil::PATTERN_SPACE.replace(base64_value, ""))
+    end
+
+    def self.decode_hex(value : String) : Bytes
+      decoded = ::IO::Memory.new
+      i = 0
+      while i < value.size - 1
+        if value[i] == '\n' || value[i] == '\r'
+          i += 1
+        else
+          current = 16 * get_hex_value(value[i]) + get_hex_value(value[i + 1])
+          decoded.write_byte(current.to_u8) if current >= 0
+          i += 2
+        end
+      end
+      decoded.to_slice
+    end
+
+    def self.get_hex_value(char : Char) : Int32
+      if char >= '0' && char <= '9'
+        char.ord - '0'.ord
+      elsif char >= 'A' && char <= 'F'
+        char.ord - 'A'.ord + 10
+      elsif char >= 'a' && char <= 'f'
+        char.ord - 'a'.ord + 10
+      else
+        -256
+      end
+    end
+
+    private def self.high_nibble(b : UInt8) : Int32
+      ((b & 0xf0_u8) >> 4).to_i
+    end
+
+    private def self.low_nibble(b : UInt8) : Int32
+      (b & 0x0f_u8).to_i
+    end
+  end
 end
